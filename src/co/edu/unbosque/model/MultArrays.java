b49a449ce68de064e8d59be495c9d8b629a5cb13
Diego Nuñez <dieguinho.n@hotmail.com>
2022-04-07 21:06:49 -0500

    MutiplicacionNormal

--- src/co/edu/unbosque/model/MultArrays.java
+++ src/co/edu/unbosque/model/MultArrays.java
@@ -9,6 +9,21 @@ public class MultArrays {
 		
 	}
 	
+	public int[][] multiplicarNormal(int[][] matriz1, int[][] matriz2){
+		int [][] matrizsol = new int [matriz1.length][matriz2[0].length];
+		if (matriz1.length == matriz2[0].length){
+			for (int i = 0; i < matriz1.length;i++) {
+				for(int j = 0; i < matriz2[0].length;j++) {
+					for(int k = 0; k <matriz1[0].length;k++) {
+						matrizsol[i][j] = matrizsol[i][j]+(matriz1[i][k]*matriz2[k][j]) ;
+					}
+				}
+			}
+		}
+		return matrizsol;
+	}
+	
+	
 	public String agregarFila(int[][] matriz, int numFila, String[] elementos, int tamX) {
 		try{
 			for (int i=0;i<tamX;i++) {
